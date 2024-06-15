d76e9401339f31e21b8e28607ab5d20f5373f072
badac <badaclock@gmail.com>
2024-06-15 13:01:01 +0900

    Revert "Add dasdasdasdasd"
    This reverts commit 78576221e28ded197f03baa94a77a7a7a64686c1.

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import './style.css';
 import javascriptLogo from './javascript.svg';
 import viteLogo from '/vite.svg';
 import { setupCounter } from './counter.js';
-// dsdasddsd
+
 async function app() {
   document.querySelector('#app').innerHTML = `
     <div>
