e73e8dccc3db662c5e70d91a602b0341722790fd
badac <badaclock@gmail.com>
2024-06-19 22:02:22 +0900

    Refactor: list-table.css 코드 수정
    검색 부분 아이콘 추가, 아이콘 위치 변경
    
    issue #9

--- list-table.js
+++ list-table.js
@@ -1,20 +1,19 @@
-const input = document.querySelector('.l-table__header__search input');
-const searchHeader = document.querySelector('.l-table__header__search');
+window.addEventListener('DOMContentLoaded', () => {
+  const listTableLayoutElem = document.querySelector('.l-table');
+  const headerSearchInputElem = document.querySelector('.l-table__header__search input');
+  const checkboxAllElem = document.querySelector('#selectAll');
+  const checkboxElem = document.querySelectorAll('tbody .c-checkbox__input');
 
-input.addEventListener('input', () => {
-  if (input.value.length > 0) {
-    searchHeader.classList.add('active');
-  } else {
-    searchHeader.classList.remove('active');
-  }
-});
-window.addEventListener('DOMContentLoaded', function () {
-  const selectAllCheckbox = document.getElementById('selectAll');
-  const checkboxes = document.querySelectorAll('tbody input[type="checkbox"]');
+  const handleHeaderSearchInput = () => {
+    listTableLayoutElem.classList.toggle('active', headerSearchInputElem.value.length > 0);
+  };
 
-  selectAllCheckbox.addEventListener('change', function () {
-    checkboxes.forEach((checkbox) => {
-      checkbox.checked = selectAllCheckbox.checked;
+  const checkAllCheckboxes = () => {
+    checkboxElem.forEach((checkbox) => {
+      checkbox.checked = checkboxAllElem.checked;
     });
-  });
+  };
+
+  headerSearchInputElem.addEventListener('input', handleHeaderSearchInput);
+  checkboxAllElem.addEventListener('change', checkAllCheckboxes);
 });
