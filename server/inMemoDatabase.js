89c56f2f2de27752269a3df88d87ba12bd4cdb2e
badac <badaclock@gmail.com>
2024-06-21 14:01:34 +0900

    Feact: 데이터베이스 연결 및 직원 조회 API 구현
    Add inMemoDatabase.js
    
    issue #24

---
+++ server/inMemoDatabase.js
@@ -0,0 +1,64 @@
+import sqlite3 from 'sqlite3';
+
+class ConnectionMaker {
+  constructor() {}
+
+  makeConnection() {
+    const db = new sqlite3.Database(`:memory:`, (err) => {
+      if (err) {
+        console.error('Error connecting to database:', err);
+      } else {
+        console.log('Connected to the in-memory SQlite database.');
+      }
+    });
+
+    return db;
+  }
+}
+
+export default class InMemoDatabase {
+  constructor() {
+    this.db = new ConnectionMaker().makeConnection();
+    this.createTable();
+  }
+
+  createTable() {
+    this.db.serialize(() => {
+      this.db.run(`
+        CREATE TABLE IF NOT EXISTS Employees (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          name TEXT NOT NULL,
+          email TEXT,
+          phone TEXT,
+          position TEXT,
+          profileImg TEXT,
+          password TEXT
+        )`);
+    });
+  }
+
+  insertEmployees(employees) {
+    (employees || []).forEach((employee) => {
+      this.insertEmployee(employee);
+    });
+  }
+
+  insertEmployee({ name, email, phone, position, profileImg }) {
+    const sql = `INSERT INTO Employees (name, email, phone, position,  profileImg) VALUES (?, ?, ?, ?, ?)`;
+    this.db.run(sql, [name, email, phone, position, profileImg], (err) => {
+      if (err) {
+        console.error('Error inserting Employee:', err);
+      }
+    });
+  }
+
+  getAllEmployees(callback) {
+    const sql = 'SELECT * FROM Employees';
+    this.db.all(sql, [], (err, rows) => {
+      if (err) {
+        console.error('Error selecting all Employees:', err);
+      }
+      callback(rows);
+    });
+  }
+}
--- server/index.js
+++ server/index.js
@@ -1,83 +1,116 @@
-import express from 'express'
-import morgan from 'morgan'
-import fs from 'fs'
-import db from './database.js'
+import express from 'express';
+import morgan from 'morgan';
+import fs from 'fs';
+import db from './database.js';
 
-const THRESHOLD = 2000
-const port = process.env.PORT || 8080
-const app = express()
+const THRESHOLD = 2000;
+const port = process.env.PORT || 8080;
+const app = express();
 
 app.use((req, res, next) => {
-  const delayTime = Math.floor(Math.random() * THRESHOLD)
+  const delayTime = Math.floor(Math.random() * THRESHOLD);
 
   setTimeout(() => {
     next();
   }, delayTime);
-})
+});
 
-app.use(morgan('dev'))
-app.use(express.static('dist'))
-app.use(express.json())
+app.use(morgan('dev'));
+app.use(express.static('dist'));
+app.use(express.json());
 
 app.get('/api/counter', (req, res) => {
-  const counter = Number(req.query.latest)
+  const counter = Number(req.query.latest);
 
   if (Math.floor(Math.random() * 10) <= 3) {
     res.status(400).send({
       status: 'Error',
       data: null,
-    })
+    });
   } else {
     res.status(200).send({
       status: 'OK',
       data: counter + 1,
-    })
+    });
   }
-})
+});
 
 app.get('/api/users.json', (req, res) => {
   fs.readFile('./server/data/users.json', 'utf8', (err, data) => {
     if (err) {
-        console.error('Error reading JSON file:', err)
-        return res.status(500).send({ 
-          status: 'Internal Server Error',
-          message: err,
-          data: null,
-        })
+      console.error('Error reading JSON file:', err);
+      return res.status(500).send({
+        status: 'Internal Server Error',
+        message: err,
+        data: null,
+      });
     }
 
     try {
-        const jsonData = JSON.parse(data)
-        res.json(jsonData)
+      const jsonData = JSON.parse(data);
+      res.json(jsonData);
     } catch (parseErr) {
-        console.error('Error parsing JSON file:', parseErr)
-        return res.status(500).send({ 
-          status: 'Internal Server Error',
-          message: parseErr,
-          data: null,
-        })
+      console.error('Error parsing JSON file:', parseErr);
+      return res.status(500).send({
+        status: 'Internal Server Error',
+        message: parseErr,
+        data: null,
+      });
     }
-  })
-})
+  });
+});
 
 app.get('/api/users', (req, res) => {
-  const sql = 'SELECT * FROM Users'
+  const sql = 'SELECT * FROM Users';
 
   db.all(sql, [], (err, rows) => {
     if (err) {
-      return res.status(500).json({ 
+      return res.status(500).json({
         status: 'Error',
-        error: err.message
-      })
+        error: err.message,
+      });
     }
 
-    res.json({ 
+    res.json({
       status: 'OK',
-      data: rows 
-    })
-  })
-})
+      data: rows,
+    });
+  });
+});
 
 app.listen(port, () => {
-  console.log(`ready to ${port}`)
-})
+  console.log(`ready to ${port}`);
+});
+
+import InMemoDatabase from './inMemoDatabase.js';
+
+//인메모리 데이터베이스, node 서버 껐다가 키면 사라지도록 db가 초기화 되도록 설정해둠.
+const indb = new InMemoDatabase();
+
+const employees = [
+  {
+    name: '홍길동',
+    email: '765idoll@gmail.com',
+    phone: '010-1234-5678',
+    position: '사원',
+    profileImg: 'https://i.imgur.com/JJEqrWE.png',
+  },
+  {
+    name: '김길동',
+    email: 'kimPhoneHi256@gmail.com',
+    phone: '010-1234-5678',
+    position: '대리',
+    profileImg: 'https://i.imgur.com/JJEqrWE.png',
+  },
+];
+
+indb.insertEmployees(employees);
+
+app.get('/api/employees', (req, res) => {
+  indb.getAllEmployees((employees) => {
+    res.json({
+      status: 'OK',
+      data: employees,
+    });
+  });
+});
