f7f1ddd9c6d7783f1f080309ccd800eec525e9b2
(MMirang) <alfkd1024@gmail.com>
2024-06-25 13:11:05 +0900

    Feat: 갤러리 컴포넌트 구현 HTML로 마크업과 구조 정의 CSS로 스타일 정의 JS 및 라우팅 작업 진행 중
    Github issue #27

---
+++ feature_gallery/gallery.sql
@@ -0,0 +1,22 @@
+CREATE TABLE card (
+    id TEXT PRIMARY KEY,
+    title TEXT,
+    date TEXT
+);
+
+INSERT INTO images (id, title, date) VALUES ('c1', '2016년 하반기 워크샵', '2016.10.01');
+INSERT INTO images (id, title, date) VALUES ('c2', '2017년 상반기 워크샵', '2017.03.01');
+INSERT INTO images (id, title, date) VALUES ('c3', '2017년 하반기 워크샵', '2017.10.01');
+INSERT INTO images (id, title, date) VALUES ('c4', '2018년 상반기 워크샵', '2018.03.01');
+INSERT INTO images (id, title, date) VALUES ('c5', '2018년 하반기 워크샵', '2018.10.01');
+INSERT INTO images (id, title, date) VALUES ('c6', '2019년 상반기 워크샵', '2019.03.01');
+INSERT INTO images (id, title, date) VALUES ('c7', '2019년 하반기 워크샵', '2019.10.01');
+INSERT INTO images (id, title, date) VALUES ('c8', '2020년 상반기 워크샵', '2020.03.01');
+INSERT INTO images (id, title, date) VALUES ('c9', '2020년 하반기 워크샵', '2020.10.01');
+INSERT INTO images (id, title, date) VALUES ('c10', '2021년 상반기 워크샵', '2021.03.01');
+INSERT INTO images (id, title, date) VALUES ('c11', '2021년 하반기 워크샵', '2021.10.01');
+INSERT INTO images (id, title, date) VALUES ('c12', '2022년 상반기 워크샵', '2022.03.01');
+INSERT INTO images (id, title, date) VALUES ('c13', '2022년 하반기 워크샵', '2022.10.01');
+INSERT INTO images (id, title, date) VALUES ('c14', '2023년 상반기 워크샵', '2023.03.01');
+INSERT INTO images (id, title, date) VALUES ('c15', '2023년 하반기 워크샵', '2023.10.01');
+INSERT INTO images (id, title, date) VALUES ('c16', '2024년 상반기 워크샵', '2024.03.01');
