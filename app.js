f0acbdc3599ab225ee1992fcedff6f482e51bc24
badac <badaclock@gmail.com>
2024-06-26 11:45:34 +0900

    Chore: EmployeeListTable 컴포넌트 위치 변경
    Merge branch 'main' into feacture/list-table
    
    Github issue #9

--- app.js
+++
@@ -1,6 +0,0 @@
-import { EmployeeListTable } from './EmployeeListTable.js';
-
-const appElem = document.querySelector('#app');
-const employeeeTable = new EmployeeListTable();
-
-appElem.appendChild(employeeeTable.elem);
