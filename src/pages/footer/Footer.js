21587b7c22474b3039450b79606883e40a26d242
Hyejin <hyejin.frontend@gmail.com>
2024-06-30 17:21:51 +0900

    chore: header, reset 스타일링 수정 및 Footer.js 마크업 더블쿼오트 텍스트수정

--- src/pages/footer/Footer.js
+++ src/pages/footer/Footer.js
@@ -1,4 +1,5 @@
 import './Footer.css';
+
 export class Footer {
   constructor(container, props) {
     this.container = container || {};
@@ -6,22 +7,22 @@ export class Footer {
   }
   render() {
     return /* HTML */ `
-      <footer class=“footer”>
-        <div class=“footer__container”>
-          <h1 class=“footer__title”>
-            <img src=“/src/assets/images/favicon/android-chrome-192x192.png” alt=“logo” />
+      <footer class="footer">
+        <div class="footer__container">
+          <h1 class="footer__title">
+            <img src="/src/assets/images/favicon/android-chrome-192x192.png" alt="logo" />
             <span>Intranet</span>
           </h1>
-          <div class=“footer__contents”>
-            <p class=“footer__copyright”>
+          <div class="footer__contents">
+            <p class="footer__copyright">
               Copyright 2024. 고낙연, 송병훈, 신혜진, 이동혁, 최미랑 all rights reserved.
             </p>
             <p>
-              <a href=“https://github.com/Dev-FE-1/team2-intranet-project-”>
+              <a href="https://github.com/Dev-FE-1/team2-intranet-project-">
                 <img
-                  class=“github__link”
-                  src=“/src/assets/images/github-mark.png”
-                  alt=“Repository Logo”
+                  class="github__link"
+                  src="/src/assets/images/github-mark.png"
+                  alt="Repository Logo"
                 />
               </a>
             </p>
