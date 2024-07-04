732c8bbdd02612e028e62601eadf6b23f957c522
Steven <ldh921126@gmail.com>
2024-07-04 17:50:26 +0900

    로그인, 마이페이지 라우팅 및 클라이언트단 기능 구현 마무리

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,71 +1,73 @@
 import { EmployeeListTable } from './employeeListTable/EmployeeListTable.js';
 // import { AttendanceList } from './attendanceList/AttendanceList.js';
-import Home from './Home';
+import HomeUpper from './Home/HomeUpper.js';
 import UserInfo from './userinfo/UserInfo';
 import Mypage from './mypage/Mypage';
 import { Layout } from './layout/Layout.js';
+import Login from './login/userLogin.js';
 
 const app = document.querySelector('#app');
 
-const layout = new Layout(app, {});
-layout.render();
+if (!sessionStorage.length) {
+  history.replaceState('', '', '/');
+  const login = new Login(app);
+  login.render();
+} else {
+  const layout = new Layout(app, {});
+  layout.render();
+  const routeView = app.querySelector('route-view');
+  window.addEventListener('popstate', router);
+  window.addEventListener('DOMContentLoaded', router);
 
-const routeView = app.querySelector('route-view');
+  const routes = {
+    '/': {
+      title: 'Home',
+      render: () => renderComponent(HomeUpper),
+    },
+    '/userinfo': {
+      title: 'userinfo',
+      render: () => renderComponentClass(UserInfo),
+    },
+    '/mypage': {
+      title: 'mypage',
+      render: () => renderComponentClass(Mypage),
+    },
+    '/employee-list': {
+      title: 'Employee List',
+      render: () => renderComponent(EmployeeListTable),
+    },
+  };
 
-const routes = {
-  '/': { title: 'Home', render: () => renderComponent(Home) },
-  '/userinfo': {
-    title: 'userinfo',
-    render: () => renderComponentClass(UserInfo),
-  },
-  '/mypage': {
-    title: 'mypage',
-    render: () => renderComponentClass(Mypage),
-  },
-  '/employee-list': {
-    title: 'Employee List',
-    render: () => renderComponent(EmployeeListTable),
-  },
-};
+  const renderComponent = (ComponentClass) => {
+    const componentInstance = new ComponentClass(routeView, {});
+    componentInstance.render();
+  };
 
-const renderComponent = (ComponentClass) => {
-  const componentInstance = new ComponentClass(routeView, {});
-  componentInstance.render();
-};
+  const renderComponentClass = (ComponentClass) => {
+    const componentInstance = new ComponentClass();
+    routeView.append(componentInstance.el);
+  };
 
-const renderComponentClass = (ComponentClass) => {
-  const componentInstance = new ComponentClass();
-  routeView.append(componentInstance.el);
-};
-
-function router() {
-  let view = routes[location.pathname];
-  if (view) {
-    document.title = view.title;
-    if (view.title === 'userinfo' || view.title === 'mypage') {
+  function router(props = {}) {
+    let view = routes[location.pathname];
+    if (view) {
+      document.title = view.title;
       routeView.innerHTML = '';
-      view.render();
+      view.render(props);
     } else {
+      history.replaceState('', '', '/');
       routeView.innerHTML = '';
-      view.render();
+      router();
     }
-  } else {
-    history.replaceState('', '', '/');
-    routeView.innerHTML = '';
-    router();
   }
+
+  // Handle navigation
+  window.addEventListener('click', (e) => {
+    const link = e.target.closest('[data-link]');
+    if (link) {
+      e.preventDefault();
+      history.pushState('', '', link.href);
+      router();
+    }
+  });
 }
-
-router();
-// Handle navigation
-window.addEventListener('click', (e) => {
-  if (e.target.matches('[data-link]')) {
-    e.preventDefault();
-    history.pushState('', '', e.target.href);
-    router();
-  }
-});
-
-// // Update router
-window.addEventListener('popstate', router);
-window.addEventListener('DOMContentLoaded', router);
