08a48612c1e2a0b48478c72e76ebdb91c87b79c4
badac <badaclock@gmail.com>
2024-07-05 20:44:12 +0900

    Chore: Update eslint config, package.json, package-lock.json, bun.lockb, server/index.js eslint 에 ignore 설정을 업데이트함

--- server/index.js
+++ server/index.js
@@ -1,66 +1,23 @@
 import express from 'express';
 import morgan from 'morgan';
-import fs from 'fs';
 import db from './database.js';
 import { indb, initializeDatabase } from './initalizeData.js';
 import history from 'connect-history-api-fallback';
 
-const THRESHOLD = 2000;
 const port = process.env.PORT || 8080;
 const app = express();
-
-app.use((req, res, next) => {
-  const delayTime = Math.floor(Math.random() * THRESHOLD);
-  setTimeout(() => {
-    next();
-  }, delayTime);
-});
 
 app.use(history());
 app.use(morgan('dev'));
 app.use(express.static('dist'));
 app.use(express.json());
 
-app.get('/api/counter', (req, res) => {
-  const counter = Number(req.query.latest);
-
-  if (Math.floor(Math.random() * 10) <= 3) {
-    res.status(400).send({
-      status: 'Error',
-      data: null,
-    });
-  } else {
-    res.status(200).send({
-      status: 'OK',
-      data: counter + 1,
-    });
-  }
+app.listen(port, () => {
+  console.log(`ready to ${port}`);
 });
 
-app.get('/api/users.json', (req, res) => {
-  fs.readFile('./server/data/users.json', 'utf8', (err, data) => {
-    if (err) {
-      console.error('Error reading JSON file:', err);
-      return res.status(500).send({
-        status: 'Internal Server Error',
-        message: err,
-        data: null,
-      });
-    }
-
-    try {
-      const jsonData = JSON.parse(data);
-      res.json(jsonData);
-    } catch (parseErr) {
-      console.error('Error parsing JSON file:', parseErr);
-      return res.status(500).send({
-        status: 'Internal Server Error',
-        message: parseErr,
-        data: null,
-      });
-    }
-  });
-});
+//employees, attendances 테이블 데이터베이스 초기화
+initializeDatabase();
 
 app.get('/api/users', (req, res) => {
   const sql = 'SELECT * FROM Users';
@@ -78,13 +35,6 @@ app.get('/api/users', (req, res) => {
     });
   });
 });
-
-app.listen(port, () => {
-  console.log(`ready to ${port}`);
-});
-
-//employees, attendances 테이블 데이터베이스 초기화
-initializeDatabase();
 
 app.get('/api/employees', (req, res) => {
   indb.getAllEmployees((employees) => {
