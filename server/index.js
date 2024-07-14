407c025f746fcaa0b3a3bdbf6de53d7bfef5d0ec
badac <badaclock@gmail.com>
2024-07-14 18:59:12 +0900

    Fix: 배포 사이트 빌드에러가 나서 server/index.js를 수정 이전으로 되돌림.

--- server/index.js
+++ server/index.js
@@ -2,14 +2,19 @@ import { container } from './container.js';
 import express from 'express';
 import history from 'connect-history-api-fallback';
 import morgan from 'morgan';
+import { fileURLToPath } from 'url';
+import path from 'path';
 import { indb, initializeDatabase } from './initalizeData.js';
 import date from 'date-and-time';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+// app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
-app.use(express.static('dist'));
 app.use(express.json());
 app.use(
   history({
@@ -21,7 +26,32 @@ app.use(
           return context.parsedUrl.pathname;
         },
       },
+      {
+        from: /^\/dist\/.*$/,
+        to: function (context) {
+          return context.parsedUrl.pathname;
+        },
+      },
     ],
+  }),
+);
+
+app.use(
+  cors({
+    origin: 'http://localhost:8080',
+    credentials: true,
+  }),
+);
+
+app.use(
+  express.static(path.join(__dirname, '../dist'), {
+    setHeaders: (res, filePath) => {
+      if (path.extname(filePath) === '.css') {
+        res.setHeader('Content-Type', 'text/css');
+      } else if (path.extname(filePath) === '.js') {
+        res.setHeader('Content-Type', 'application/javascript');
+      }
+    },
   }),
 );
 
